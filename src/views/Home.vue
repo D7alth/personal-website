acf377ae0876f6a0d4fa3a235675244db5508766
alberth henrique <alberthhls@protonmail.com>
2024-05-29 23:03:14 -0300

    🎉 Start portfolio project

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>FullStack Developer</h1>
+</template>
+
+<style>
+@font-face {
+  font-family: 'GalleryModern';
+  src: url('@/assets/fonts/GalleryModern.otf') format('opentype');
+  font-weight: normal;
+  font-style: normal;
+}
+h1 {
+  font-family: 'GalleryModern', sans-serif;
+  font-size: 6.6rem;
+  font-weight: 500;
+  text-align: center;
+  margin-top: 2rem;
+  color: var( --vt-c-white);
+}
+</style>
