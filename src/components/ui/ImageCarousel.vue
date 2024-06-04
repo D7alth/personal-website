363ca00949b69000b924c2cf7e5971639bead956
alberth henrique <alberthhls@protonmail.com>
2024-06-03 23:02:58 -0300

    🚧 Added new coreTechnologies logos and working in new section

--- src/components/ui/ImageCarousel.vue
+++ src/components/ui/ImageCarousel.vue
@@ -1,11 +1,47 @@
 <script setup lang="ts">
-
+const props = defineProps<{
+  items: {
+    key: string;
+    src: string;
+    alt: string;
+  }[];
+}>();
 </script>
 
 <template>
-  $END$
+  <div class="carousel">
+      <div class="carousel-item" v-for="item in props.items" :key="item.key">
+        <img :src="item.src" :alt="item.alt">
+      </div>
+  </div>
 </template>
 
 <style scoped>
 
+@keyframes slide {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+.carousel {
+  scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
+  scroll-behavior: smooth;
+  overflow: hidden;
+  white-space: nowrap;
+  padding: 4rem 0;
+  max-width: 600px;
+}
+.carousel-item {
+  display: inline-block;
+  animation: 3s slide infinite;
+}
+img {
+  height: 4rem;
+  filter: invert(1);
+  margin: 0 3rem;
+}
 </style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Hero from "@/views/Home/Hero.vue";
+import CoreTechnologies from "@/views/Home/CoreTechnologies.vue";
+</script>
+
+<template>
+  <div class="home">
+    <Hero/>
+    <CoreTechnologies/>  
+  </div>
+</template>
+
+<style scoped>
+.home
+{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  
+}
+</style>
